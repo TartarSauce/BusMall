e9230a1b2995076541fc414564670534b23a9daa
TartarSauce <uma_bangalore@hotmail.com>
2016-05-23 23:14:30 -0700

    updated image sizes and cleaned up display

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
-// - Need to know how many times a particular image was shown
-// - Need to know how many times an image was clicked on
-// - Need to get the above data on all 25 images over several focus group participants
-// - Clear flow of logic
-// - Each image should have the following information attached to it
-//   - relative path
-//   - name to be used for reporting
-//   - number of times clicked - numClicks
-//   - number of times shown - numShown
-// - Keep an array object to store all images as one unit
-// - This happens 25 times - generate a set of three random images, no duplicates
-// - Increment the numClicks every time an image makes it to the set
-// - Get a handle to the container element on the web page
-//   - Populate it with the three images
-//   - Add an event listener to the container element on the web page to listen for clicks
-//   - Define an event handler for the event
-//     - Figure out which image was clicked on
-//     - register that information
-//     - if still within the 25 set limit, display the next set of three images
-//     - if crossed the 25 set limit, stop listening to events on the container element
-// - Report to the user that the survey is over
-// - Show graphs when that data becomes available
-
+//---------------------------------
+// GLOBAL VARIABLES
+//---------------------------------
+// handy array of image names
 var allNames = ['bag', 'boots', 'chair', 'dragon', 'scissors', 'tauntaun',
                     'usb', 'banana', 'breakfast', 'cthulhu', 'pen', 'shark', 'unicorn',
                     'water-can', 'bathroom', 'bubblegum', 'dog-duck', 'pet-sweep',
                     'sweep', 'wine-glass'];
+
+// the array that will hold all image objects
 var allImages = [];
 
-var containerElement = document.getElementById('threeimgset');
+// track how many times the trio of images are shown
+var counter25 = 0;
+
+// handle to DOM elements containing images
 var imageElement1 = document.getElementById('img1');
 var imageElement2 = document.getElementById('img2');
 var imageElement3 = document.getElementById('img3');
 
-// Class definition
+//---------------------------------
+// CLASS DEFINITION
+//---------------------------------
 function SurveyImage (name) {
   this.path = 'imgs/' + name + '.jpg';
   this.name = name;
@@ -41,36 +29,106 @@ function SurveyImage (name) {
   allImages.push(this);
 };
 
+//---------------------------------
+// PROTOTYPE FUNCTIONS
+//---------------------------------
+// update the number of clicks
+SurveyImage.prototype.updateClicks = function() {
+  this.numClicks++;
+};
+
+// update the number of times the image is shown to user
+SurveyImage.prototype.updateShown = function() {
+  this.numShown++;
+};
+
+// retrieve the image name
+SurveyImage.prototype.getName = function() {
+  return this.name;
+};
+
+//---------------------------------
+// OTHER SUPPORTING FUNCTIONS, EVENT HANDLERS
+//---------------------------------
+// display a random set of 3 images
+function displayRandomImages() {
+  counter25++;  // track counter to 25
+
+  if (counter25 < 26) {
+    var randIndex1 = Math.floor(Math.random() * (allImages.length - 0));
+    var randIndex2 = Math.floor(Math.random() * (allImages.length - 0));
+    while (randIndex1 === randIndex2) {
+      var randIndex2 = Math.floor(Math.random() * (allImages.length - 0));
+    }
+    var randIndex3 = Math.floor(Math.random() * (allImages.length - 0));
+    while ((randIndex1 === randIndex3) || (randIndex2 === randIndex3)) {
+      var randIndex3 = Math.floor(Math.random() * (allImages.length - 0));
+    }
+    console.log(randIndex1, randIndex2, randIndex3);
+
+    // display all images on screen
+    imageElement1.src = allImages[randIndex1].path;
+    imageElement2.src = allImages[randIndex2].path;
+    imageElement3.src = allImages[randIndex3].path;
+
+    // increment numShown on all those images
+    allImages[randIndex1].updateShown();
+    allImages[randIndex2].updateShown();
+    allImages[randIndex3].updateShown();
+  } else {
+    displayStats();
+  }
+}
+
+// show stats to console when done with 25, reset counter25
+function displayStats() {
+  for (var i = 0; i < allImages.length; i++) {
+    console.log(allImages[i].name + ' had numClicks ' + allImages[i].numClicks
+    + ' and numShown ' + allImages[i].numShown);
+  }
+  counter25 = 0;
+}
+
+// Event handler for the click event
+function handleClick(event) {
+  // get the image element that was clicked on
+  var clickedElement = document.getElementById(event.target.id);
+
+  // get the path attached to the image element, split the string
+  var pathArray = ((clickedElement.src).split('/'));
+
+  // get the last element which is the name of the jpg file
+  var length = pathArray.length;
+
+  // remove the .jpg suffix to get only the name
+  var imgName = pathArray[length - 1].split('.');
+  console.log(imgName[0]);
+
+  // loop through the image array, find image object corresponding
+  // to the one user clicked on, and increment click count
+  for (var i = 0; i < allImages.length; i++) {
+    if (imgName[0] === allImages[i].getName()) {
+      var clickIndex = i;
+      allImages[i].updateClicks();
+    }
+  }
+
+  // display next set of images
+  displayRandomImages();
+}
+
+//---------------------------------
+// MAIN SECTION OF EXECUTABLE CODE
+//---------------------------------
 // create the array of images
 for (var i = 0; i < allNames.length; i++) {
   var newImg = new SurveyImage(allNames[i]);
 }
 
-// get a random set of 3 indices
-var randIndex1 = Math.floor(Math.random() * ((allImages.length - 0)));
-var randIndex2 = Math.floor(Math.random() * ((allImages.length - 0)));
-while (randIndex1 === randIndex2) {
-  var randIndex2 = Math.floor(Math.random() * ((allImages.length - 0)));
-}
-var randIndex3 = Math.floor(Math.random() * ((allImages.length - 0)));
-while ((randIndex1 === randIndex3) || (randIndex2 === randIndex3)) {
-  var randIndex3 = Math.floor(Math.random() * ((allImages.length - 0)));
-}
+// display the first set of random images
+displayRandomImages();
 
-// display all images on screen
-imageElement1.src = allImages[randIndex1].path;
-imageElement2.src = allImages[randIndex2].path;
-imageElement3.src = allImages[randIndex3].path;
-
-//console.log(allImages);
-console.log(randIndex1, randIndex2, randIndex3);
-// Attach event handler to the container holding the images
-//containerElement.addEventListener('click', handleClick);
-
+// Attach event handler to the containers holding the images
 imageElement1.addEventListener('click', handleClick);
-
-// Event handler for the click event
-function handleClick(event) {
-  console.log(event.target.id);
-  var s = document.getElementById(event.target.id);
-}
+imageElement2.addEventListener('click', handleClick);
+imageElement3.addEventListener('click', handleClick);
